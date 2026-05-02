c5a0b6c7b470cad61f65cebc4a17e9a37833d2a9
Mattia <darksoulcyber@hotmail.it>
2026-05-02 16:22:06 +0200

    Configure GitHub Pages build

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,39 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/YuGiOhDecklistGenerator/",
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "vendor-react",
+              test: /[\\/]node_modules[\\/](?:react|react-dom|scheduler)[\\/]/,
+            },
+            {
+              name: "vendor-canvas",
+              test: /[\\/]node_modules[\\/](?:konva|react-konva)[\\/]/,
+            },
+            {
+              name: "vendor-icons",
+              test: /[\\/]node_modules[\\/]lucide-react[\\/]/,
+            },
+            {
+              name: "vendor-archive",
+              test: /[\\/]node_modules[\\/]jszip[\\/]/,
+            },
+            {
+              name: "vendor-validation",
+              test: /[\\/]node_modules[\\/]zod[\\/]/,
+            },
+            {
+              name: "vendor-state",
+              test: /[\\/]node_modules[\\/]zustand[\\/]/,
+            },
+          ],
+        },
+      },
+    },
+  },
 });
